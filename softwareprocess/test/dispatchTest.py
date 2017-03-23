5a0633e489ec7eaee3dd6816c553ea88c0228560
nirmitpatel <nap0017@tigermail.auburn.edu>
2017-03-22 23:56:08 -0500

    assignment4

---
+++ softwareprocess/test/dispatchTest.py
@@ -0,0 +1,39 @@
+import unittest
+import softwareprocess.dispatch as DP
+import math
+
+
+class dispatchTest(unittest.TestCase):
+
+# -----------------------------------------------------------------------
+# ---- Acceptance Tests
+# 100 dipatch function
+#    Desired level of confidence:    boundary value analysis
+#    Input-output Analysis
+#        inputs:
+#        outputs:
+#    Happy path analysis:
+#           'op':'predict'
+#           'op':'correct'
+#           'op':'locale'
+#    Sad path analysis:
+#
+# Happy path
+
+    def test100_010_ShouldReturnSameDictionary(self):
+        sighting={'op':'predict'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(sighting,mySample)
+    def test100_020_ShouldReturnSameDictionary(self):
+        sighting={'op':'correct'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(sighting,mySample)
+    def test100_030_ShouldReturnSameDictionary(self):
+        sighting={'op':'locale'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(sighting,mySample)
+    def test100_040_ShouldReturnError(self):
+        sighting={}
+        result={'error':'no op  is specified1'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(sighting,mySample)
