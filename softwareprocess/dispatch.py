838520d2a129617a01e854c62bd8040a52d21fe5
Nirmit <Nirmit@DESKTOP-SB2Q1SQ>
2017-03-23 19:27:16 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -2,6 +2,7 @@ import math
 
 def dispatch(values=None):
 
+    String1="0123456789"
     #Validate parm
     if(values == None):
         return {'error': 'parameter is missing'}
@@ -24,6 +25,55 @@ def dispatch(values=None):
             if("d" not in obv):
                 values['error'] = 'observation is invalid'
                 return values
+            obvarray=[]
+            for x in obv.split("d"):
+                #print(x)
+                obvarray.append(x)
+            #print(obvarray)
+            if(obvarray[0]==""):
+                values['error'] = 'observation is invalid'
+                return values
+                #return {'error': 'observation is invalid'}}
+            if(obvarray[1]==""):
+                values['error'] = 'observation is invalid'
+                return values
+                #return {'error': 'observation is invalid'}
+            for ch in obvarray[0]:
+                if(ch not in String1):
+                    values['error'] = 'observation is invalid'
+                    return values
+                    #return {'error': 'observation is invalid'}
+
+            if(int(obvarray[0])>=int(90) or int(obvarray[0])<0 ):
+                values['error'] = 'observation is invalid'
+                return values
+                #return {'error': 'observation is invalid'}
+            if("." not in obvarray[1]):
+                values['error'] = 'observation is invalid'
+                return values
+                #return {'error': 'observation is invalid'}
+            minutearray=[]
+            for x in obvarray[1].split("."):
+                #print(x)
+                minutearray.append(x)
+            if(minutearray[0]==""):
+                values['error'] = 'observation is invalid'
+                return values
+                #return {'error': 'observation is invalid'}
+            if(minutearray[1]==""):
+                values['error'] = 'observation is invalid'
+                return values
+                #return {'error': 'observation is invalid'}
+            for ch in minutearray[0]:
+                if(ch not in String1):
+                    values['error'] = 'observation is invalid'
+                    return values
+                    #return {'error': 'observation is invalid'}
+            for ch in minutearray[1]:
+                if(ch not in String1):
+                    values['error'] = 'observation is invalid'
+                    return values
+                    #return {'error': 'observation is invalid'}
 
         #newCode - End
         return values    #<-------------- replace this with your implementation
