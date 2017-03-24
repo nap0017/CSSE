11240f53ece45e14b6e90a46919f99c2c51db09b
Nirmit <Nirmit@DESKTOP-SB2Q1SQ>
2017-03-23 19:47:42 -0500

    Red Light |

--- softwareprocess/test/dispatchTest.py
+++ softwareprocess/test/dispatchTest.py
@@ -119,3 +119,8 @@ class dispatchTest(unittest.TestCase):
         result={'error': 'pressure is invalid'}
         mySample = DP.dispatch(sighting)
         self.assertEqual(result['error'],mySample['error'])
+    def test300_040_ShouldReturnError(self):
+        sighting={'op':'adjust','observation': '80d59.0','height':'20','temperature':'10','pressure':'10','horizon':'10a'}
+        result={'error': 'pressure is invalid'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(result['error'],mySample['error'])
