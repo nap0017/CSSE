90fa69c2e85b226b8ce308ff2493051b3157e2db
nirmitpatel <nap0017@tigermail.auburn.edu>
2017-04-07 01:15:34 -0500

    Red Light |

--- softwareprocess/test/dispatch_predict_Test.py
+++ softwareprocess/test/dispatch_predict_Test.py
@@ -70,3 +70,10 @@ class dispatch_predict_Test(unittest.TestCase):
         result={'op':'predict', 'body': 'Betelgeuse', 'date': '2016-99-17', 'time': '03:15:42', 'error':'invalid date'}
         mySample = DP.dispatch(sighting)
         self.assertEqual(result['error'],mySample['error'])
+
+    def test200_007_ShouldReturnTimeError(self):
+        sighting={'op':'predict', 'body': 'Betelgeuse', 'date': '20016-01-17', 'time': '03:99:42'}
+        result={'op':'predict', 'body': 'Betelgeuse', 'date': '2016-99-17', 'time': '03:15:42', 'error':'invalid date'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(result['error'],mySample['error'])
+
