89d8245079cc8f6d1bf2da1bc51ad5bd43ab0149
nirmitpatel <nap0017@tigermail.auburn.edu>
2017-04-26 20:47:32 -0500

    Red Light |

--- softwareprocess/test/dispatch_correct_Test.py
+++ softwareprocess/test/dispatch_correct_Test.py
@@ -50,3 +50,9 @@ class dispatch_correct_Test(unittest.TestCase):
         result={'error':'invalid altitude', 'op':'correct','lat':'16d32.3' , 'long':'95.41.6', 'altitude':15,  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         mySample = DP.dispatch(sighting)
         self.assertEqual(result['error'],mySample['error'])
+
+    def test200_005_ShouldReturnError(self):
+        sighting={'op':'correct', 'lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
+        result={'error':'invalid lat','op':'correct', 'lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
+        mySample = DP.dispatch(sighting)
+        self.assertEqual(result['error'],mySample['error'])
