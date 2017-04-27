8c026f764f359bf5a9f979ca9fe0252df5633080
nirmitpatel <nap0017@tigermail.auburn.edu>
2017-04-26 20:39:19 -0500

    Green Light |

--- softwareprocess/test/dispatch_correct_Test.py
+++ softwareprocess/test/dispatch_correct_Test.py
@@ -40,7 +40,7 @@ class dispatch_correct_Test(unittest.TestCase):
         self.assertEqual(result['error'],mySample['error'])
 
     def test200_002_ShouldReturnError(self):
-        sighting={'op':'correct','lat':16d32.3, 'long':'95.41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
+        sighting={'op':'correct','lat': 16 , 'long':'95.41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
         result={'error':'mandatory information is missing', 'op': 'correct'}
         mySample = DP.dispatch(sighting)
         self.assertEqual(result['error'],mySample['error'])
